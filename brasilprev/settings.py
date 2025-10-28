47a94c29267375f06cbc0fcc3905badd67cfc559
Vanderson <vandersondev@gmail.com>
2025-10-28 15:57:59 -0300

    Update settings.py

--- brasilprev/settings.py
+++ brasilprev/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'brasilprev.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres12345',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
